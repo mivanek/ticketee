e1a6e92e9d24f0eeccc85fc05b44ebcbfd2ea007
Marko Ivanek <markoivanek@gmail.com>
2013-06-19 18:36:53 +0200

    Add title functionality to pages

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ end
 
 group :test, :development do
   gem 'rspec-rails', '~> 2.5'
+  gem 'pry-rails'
 end
 
 group :test do
--- features/step_definitions/web_definitions.rb
+++ features/step_definitions/web_definitions.rb
@@ -7,7 +7,7 @@ When /^I follow "New Project"$/ do
 end
 
 And /^I fill in "Name" with "TextMate 2"$/ do
-  fill_in('Name', with: 'TextMate2')
+  fill_in('Name', with: 'TextMate 2')
 end
 
 And /^I press "Create Project"$/ do
@@ -16,4 +16,13 @@ end
 
 Then /^I should see "Project has been created"$/ do
   page.should have_content "Project has been created"
+end
+
+And /^I should be on the project page for "(.+)"$/ do |page|
+  id = Project.find_by_name(page).id
+  visit project_path(id)
+end
+
+And /^I should see the "(.+)" title$/ do |title|
+  page.should have_title title
 end
